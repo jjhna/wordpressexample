1a3f009c3a77f70128e9b07f3d0779e4ca1533f0
Jon <22411356+jjhna@users.noreply.github.com>
2021-07-21 13:55:44 -0400

    Search and rest API
    Learned about search bars and using rest API. However as before I needed to copy from the lesson files. Also later on in the course a non JQuery example will be implemented and needed to be copy so refraining from commenting at the moment. However the search feature is finally working.

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -1,58 +1,71 @@
-<?php 
-    //Used to make custom changes to the event type posts. Make sure to go to admin page -> settings/peramlinks and click save changes after
-    // making any changes below
-    function university_post_types() {
-        //event post type
-        register_post_type('event', array(
-            'show_in_rest' => true,
-            'supports' => array('title', 'editor', 'excerpt'),
-            'rewrite' => array('slug' => 'events'),
-            'has_archive' => true,
-            'public' => true,
-            'show_in_rest' => true,
-            'labels' => array(
-                'name' => 'Events',
-                'add_new_item' => 'Add New Event',
-                'edit_item' => 'Edit Event',
-                'all_items' => 'All Events',
-                'singular_name' => 'Event'
-            ),
-            'menu_icon' => 'dashicons-calendar'
-        ));
+<?php
 
-        //program post type
-        register_post_type('program', array(
-            'show_in_rest' => true,
-            'supports' => array('title', 'editor'),
-            'rewrite' => array('slug' => 'programs'),
-            'has_archive' => true,
-            'public' => true,
-            'show_in_rest' => true,
-            'labels' => array(
-                'name' => 'Program',
-                'add_new_item' => 'Add New Program',
-                'edit_item' => 'Edit Program',
-                'all_items' => 'All Programs',
-                'singular_name' => 'Program'
-            ),
-            'menu_icon' => 'dashicons-awards'
-        ));
+function university_post_types() {
+  // Campus Post Type
+  register_post_type('campus', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array('slug' => 'campuses'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Campuses',
+      'add_new_item' => 'Add New Campus',
+      'edit_item' => 'Edit Campus',
+      'all_items' => 'All Campuses',
+      'singular_name' => 'Campus'
+    ),
+    'menu_icon' => 'dashicons-location-alt'
+  ));
+  
+  // Event Post Type
+  register_post_type('event', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt'),
+    'rewrite' => array('slug' => 'events'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
+  ));
 
-        //professor post type
-        register_post_type('professor', array(
-            'show_in_rest' => true,
-            'supports' => array('title', 'editor', 'thumbnail'),
-            'public' => true,
-            'show_in_rest' => true,
-            'labels' => array(
-                'name' => 'Professor',
-                'add_new_item' => 'Add New Professor',
-                'edit_item' => 'Edit Professor',
-                'all_items' => 'All Professors',
-                'singular_name' => 'Professor'
-            ),
-            'menu_icon' => 'dashicons-welcome-learn-more'
-        ));
-    }
+  // Program Post Type
+  register_post_type('program', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
 
-    add_action('init', 'university_post_types');
+  // Professor Post Type
+  register_post_type('professor', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => true,
+    'labels' => array(
+      'name' => 'Professors',
+      'add_new_item' => 'Add New Professor',
+      'edit_item' => 'Edit Professor',
+      'all_items' => 'All Professors',
+      'singular_name' => 'Professor'
+    ),
+    'menu_icon' => 'dashicons-welcome-learn-more'
+  ));
+}
+
+add_action('init', 'university_post_types');
--- themes/fictional-university-theme/build/index.asset.php
+++ themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => 'f9f72a2c8f3ee7c67e4305388b448bb9');
+<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '864fe3441899e15dac0eb7657c01fe92');
